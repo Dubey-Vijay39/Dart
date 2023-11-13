829a85bdcc45e231a3b30dd375f8deb5cd148537
Vijay <ashud616@gmail.com>
2023-11-13 13:15:45 +0530

    Ok

---
+++ data_types.dart
@@ -0,0 +1,14 @@
+// There are total 7 built in data types in dart
+// They are List,number,map,string, runes, symbols, booleans.
+// Here is the examples
+void main(){
+  print("This is string");
+  print(4+3);
+  var name = "Vijay";
+  var age = 21;
+  print(age);
+}
+// var keyword in Dart is used for declaring the value as everything is object in dart 
+// dart assumes the data type by checking the right end value of right side item
+
+// Note: All data types in dart are object therefore there default value is Null
