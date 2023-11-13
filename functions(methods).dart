a40fc268c360884a121869f6b3c3dead85ebbedc
Vijay <ashud616@gmail.com>
2023-11-13 19:41:12 +0530

    Notes

---
+++ functions(methods).dart
@@ -0,0 +1,15 @@
+// Return Type in dart is optional, but it is a good practice to use a return type
+// Structure of Function
+
+// int area(int l, int b){
+// function body
+// return l*b;
+// }  here int at the start is return type
+// area is function name and l and b are the parameters
+
+// In dart everything is function and so does the function
+
+void main(){
+  var name = "v";//local variable
+  print(name*5);
+}
