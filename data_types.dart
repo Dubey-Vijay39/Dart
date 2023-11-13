6b2a898fb128b813c9ca6dbcf3179bacae3ebafc
Vijay <ashud616@gmail.com>
2023-11-13 13:46:51 +0530

    all About string and number

--- data_types.dart
+++ data_types.dart
@@ -7,8 +7,19 @@ void main(){
   var name = "Vijay";
   var age = 21;
   print(age);
-}
+  var n;
+  print(n);
+
 // var keyword in Dart is used for declaring the value as everything is object in dart 
 // dart assumes the data type by checking the right end value of right side item
 
 // Note: All data types in dart are object therefore there default value is Null
+
+// In dart we dont use + Symbol to add two strings it is considered to be bad practice
+// Therefore we use sting interpolation that means we use dollar sign($) before a variable to declare it
+  print("My name is $name and my age is $age"); // This is string interpolation
+// For clear example use ["my name is $name "] instead of ["my name is" + name]
+
+}
+
+
